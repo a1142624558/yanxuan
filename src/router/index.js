647af7de021562d18ac2e36b8e9d0a50fce0fce7
a114264558 <1142624558@qq.com>
2020-07-20 10:35:15 +0800

    a

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
-
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 //引入自定义的路由文件
 import shop from "./shop";
+
+//study相关信息
+import lazyload from "@/views/study/lazyload";
+import waterfall from "@/views/study/waterfall";
 
 Vue.use(Router)
 
@@ -15,7 +18,22 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld
     },
+    {
+      path: '/lazyload',
+      name: 'lazyload',
+      component: lazyload,
+      meta:{
+        title:"图片懒加载"
+      }
+    },{
+      path: '/waterfall',
+      name: 'waterfall',
+      component: waterfall,
+      meta:{
+        title:"瀑布流布局"
+      }
+    },
     //扩展运算符
-    ...shop 
+    ...shop
   ]
 })
